6b859f6c058113fba4bc5a64582f0e1c5dc7a8a0
Coen Büller <c.m.buller@student.tudelft.nl>
2025-04-16 22:05:11 +0200

    wrote some code that adds the commonvoice clips to the non_wakeword map

--- Data_Handler/add_clips_to_non_wakeword.py
+++ Data_Handler/add_clips_to_non_wakeword.py
@@ -1,15 +1,21 @@
 import os
+import librosa as lb
+from scipy.io.wavfile import write
+import numpy as np
+from random import random
+from tqdm import tqdm
 
-root = 'C:\Users\coenb\Coen_bestanden\home_assistent\sound_data\wake_word_audio'
-path = os.path.join(root, 'non_wake_word')
 
-try: 
-    os.makedirs(path)
-except FileExistsError:
-    print('Directory already exists.')
+root = 'C:\\Users\\coenb\\Coen_bestanden\\home_assistent\\sound_data\\audio_files\\clips'
 
-background_audio = 'C:\Users\coenb\Coen_bestanden\home_assistent\sound_data\background_audio_segments'
-for audio in os.listdir(background_audio):
-    if audio.endswith('.wav'):
-        os.rename(os.path.join(root, audio), os.path.join(path, audio))
-        print(f"Moved {audio} to {path}")
+output_root = 'C:\\Users\\coenb\\Coen_bestanden\\home_assistent\\sound_data\\wake_word_audio\\non_wake_word_map'
+audio_files = os.listdir(root)
+audio_files_array = np.array(audio_files)
+
+random_indices = np.random.choice(len(audio_files_array), size=3000, replace=False)
+random_files = audio_files_array[random_indices]
+for file in tqdm(random_files):
+    file_path = os.path.join(root, file)
+    y, sr = lb.load(file_path, sr=16000) 
+    output_path = os.path.join(output_root, file.strip('.mp3'))
+    write(output_path + '.wav',sr, y)  # Save the audio file with the same sample rate
