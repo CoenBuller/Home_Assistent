adding softmax to outputs of model, made more strict requirements for correct guess and added the loss value to the 'accuracy' function as output

Wake_Word_Detector/test_wake_word_detector.py
@@ -25,8 +25,9 @@ def process_audio(audio_data, model, i):
         output = model(mfcc).squeeze(0)  # Add batch dimension
 
     output = torch.softmax(output, dim=1)  # Apply softmax to get probabilities
-    max_prob, predicted_class = torch.max(output, 1)  # Get the predicted class and max probability
-    if predicted_class == 1 and max_prob > 0.99 and (output[:,1] - output[:,0]) > 0.5:  # Assuming 1 is the class for the wake word
+    max_prob, predicted_class = torch.max(output, 1)
+    min_prob, _ = torch.min(output, 1)  # Get the predicted class and max probability
+    if predicted_class == 1 and max_prob > 0.99 and min_prob <0.01:  # Assuming 1 is the class for the wake word
         print(output)
         print("Hello there! Jarvis is the G listening to you! wagwan bruv!")
         wav.write(f"C:\\Users\\coenb\\Coen_bestanden\\home_assistent\\Home_Assistent\\\\audio_sample_{int(i)}.wav", SAMPLE_RATE, audio_data)

Wake_Word_Detector/train_wake_word_gru.py
@@ -19,19 +19,24 @@ model.to(device)
 
 optimizer = optim.AdamW(model.parameters(), lr=0.001)
 criterion = torch.nn.BCEWithLogitsLoss()
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2)
 
-def accuracy(model, data_loader):
+def accuracy(model, data_loader, criterion):
     model.eval()
     correct = 0
     total = 0
+    loss = 0
     with torch.no_grad():
         for inputs, _, labels in tqdm(data_loader):
-            inputs = inputs.squeeze(0)
             labels = labels.float()
             outputs = model(inputs.permute(2, 0, 1)).squeeze(0)
+            loss += criterion(outputs, labels)
             total += labels.size(0)
-            correct += (outputs.argmax(1) == labels.argmax(1)).sum().item()
-    return correct / total
+            outputs = torch.softmax(outputs, 1)
+            max_value, max_idx = torch.max(outputs, 1)
+            min_value, _ = torch.min(outputs, 1)
+            correct += ((max_idx==labels.argmax(1)) & (max_value>0.99) & (min_value<0.01)).sum().item()
+    return correct / total, loss/total
 
 epochs = 50
 for epoch in range(epochs):
@@ -44,15 +49,18 @@ for epoch in range(epochs):
         outputs = outputs.squeeze(0)
         loss = criterion(outputs, labels)
         loss.backward()
+        ouptuts = torch.softmax(outputs, 1)
         optimizer.step()
         
-        print(f'Epoch [{epoch+1}/{epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.8f}, Accuracy: {(outputs.argmax(1) == labels.argmax(1)).float().mean().item():.4f}')
+        max_val, max_idx = torch.max(outputs, 1)
+        min_val, _ = torch.min(outputs, 1)
+        print(f'Epoch [{epoch+1}/{epochs}], Step [{i+1}/{len(train_loader)}], Loss: {loss.item():.8f}, Accuracy: {((max_idx==labels.argmax(1)) & (max_val>0.95) & (min_val<0.1)).float().mean().item():.4f}')
     
-    accuracy_value = accuracy(model, train_loader)
-    scheduler.step(accuracy_value)
+    accuracy_value, avg_loss = accuracy(model, train_loader, criterion)
+    scheduler.step(avg_loss)
     print(f'Epoch: {epoch+1}, Accuracy {accuracy_value:.4f}')
     torch.save(model.state_dict(), 'Models\\wakeWord_detector_model.pth')
-    if accuracy_value > 0.995:
+    if accuracy_value > 0.999:
         print("Model accuracy is above 99.5%, stopping training.")
         break
 